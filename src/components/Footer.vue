b64442a7adb3076235a7c50c377491964e9e9978
노은신 <nes0410@i-gns.co.kr>
2021-11-18 07:30:27 +0900

    refs #4 create footer wireframe

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footerOutline">
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footerOutline {
+  border-style: solid;
+  width: 100%;
+  height: 102px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+footer {
+  border-style: solid;
+  width: 192px;
+  height: 102px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
