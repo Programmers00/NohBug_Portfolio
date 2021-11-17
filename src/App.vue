5e884894e3c94384c2bf478a1b4ccd64f137871f
노은신 <nes0410@i-gns.co.kr>
2021-11-18 07:24:52 +0900

    refs #1 create Main page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppMain/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppMain from './components/AppMain.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppMain
   }
 }
 </script>
@@ -21,6 +19,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  width: 1440px;
+  height: 994;
 }
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header/>
+    <Section/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+import Section from './Section'
+import Footer from './Footer.vue'
+export default {
+  name: 'AppMain',
+  components: {
+    Header,
+    Section,
+    Footer
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
