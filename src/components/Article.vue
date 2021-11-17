3c3f6a3bdcc5c8904a600c9b9e4c0ec43b8f410a
노은신 <nes0410@i-gns.co.kr>
2021-11-18 07:31:25 +0900

    refs #5 create article wireframe

---
+++ src/components/Article.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    About
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Article'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+article {
+  border-style: solid;
+  width: 588px;
+  height: 323px;
+}
+</style>
