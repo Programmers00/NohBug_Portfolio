408f57bcd0318988219cc90e4675e965a6a196f9
노은신 <nes0410@i-gns.co.kr>
2021-11-18 07:32:18 +0900

    refs #6 create picture wireframe

---
+++ src/components/Picture.vue
@@ -0,0 +1,28 @@
+<template>
+  <picture>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png" alt="Image">
+  </picture>
+</template>
+
+<script>
+export default {
+  name: 'Picture'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+picture {
+  border-style: solid;
+  width: 524px;
+  height: 524px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+img {
+  width: 100%;
+  border-radius: 4px;
+  padding: 4px;
+}
+</style>
