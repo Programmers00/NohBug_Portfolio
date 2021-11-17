6e1923b67f41a66e774d650bde546e8b7f0c3205
노은신 <nes0410@i-gns.co.kr>
2021-11-18 07:27:50 +0900

    refs #2 create header wireframe

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <p>Header</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  border-style: solid;
+  width: 1440px;
+  height: 56px;
+}
+</style>
