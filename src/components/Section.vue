78e33eca9ea34ff81b7c2f497ccaf122b3427d68
노은신 <nes0410@i-gns.co.kr>
2021-11-18 07:29:17 +0900

    refs #3 create section wireframe

---
+++ src/components/Section.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <div class="content">
+      <Article/>
+      <Picture/>
+    </div>
+  </section>
+</template>
+
+<script>
+import Article from './Article'
+import Picture from './Picture'
+export default {
+  name: 'Section',
+  components: {
+    Article,
+    Picture
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+section {
+  border-style: solid;
+  width: 100%;
+  height: 706px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content {
+  border-style: solid;
+  width: 1200px;
+  height: 524px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
