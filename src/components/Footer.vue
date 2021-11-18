a8e0064fdd27d0ba014e88ce2d483d85fc08f716
노은신 <nes0410@i-gns.co.kr>
2021-11-19 08:53:16 +0900

    refs #12 add Icons and Logo in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="footerOutline">
+  <div class="footer__Outline">
     <footer>
-      footer
+      <ul>
+        <a href=""><i class="fab fa-instagram"></i></a>
+        <a href=""><i class="fab fa-facebook-square"></i></a>
+        <a href=""><i class="fas fa-at"></i></a>
+      </ul>
+      <div class="navbar__logo">
+      <i class="fas fa-bug"></i>
+      <a href="#">NohBug</a>
+    </div>
     </footer>
   </div>
 </template>
@@ -14,7 +22,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.footerOutline {
+a {
+  text-decoration: none;
+  color: #263343;
+}
+.footer__Outline {
   border-style: solid;
   height: 102px;
   display: flex;
@@ -22,11 +34,28 @@ export default {
   align-items: center;
 }
 footer {
+  flex-direction: column;
   border-style: solid;
   width: 192px;
   height: 102px;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+footer ul {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  font-size: 24px;
+  color: #263343;
+  list-style: none;
+  padding-left: 0;
+}
+.navbar__logo {
+  font-size: 16px;
+  color: whitesmoke;
+}
+.navbar__logo i{
+  color: #d49466;
 }
 </style>
