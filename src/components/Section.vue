3ec58210cf63234503e7629983bdff1132db4954
노은신 <nes0410@i-gns.co.kr>
2021-11-22 19:47:13 +0900

    refs #13 remove height, add wrap, space-around

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -23,7 +23,6 @@ export default {
 <style scoped>
 section {
   border-style: solid;
-  height: 706px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -31,9 +30,9 @@ section {
 .content {
   border-style: solid;
   width: 1200px;
-  height: 524px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex-wrap: wrap;
 }
 </style>
