879d4fd3ffaef62d9ccdabbd0bbdcab38f3dc56d
노은신 <nes0410@i-gns.co.kr>
2021-11-23 07:41:00 +0900

    refs #17 create projects sction

---
+++ src/components/Projects.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div class="content">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+}
+.content {
+  width: 1200px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+}
+</style>
