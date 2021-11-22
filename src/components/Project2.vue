4493daedb9025fd6d61b1162e5ef6bb5aca7feb0
노은신 <nes0410@i-gns.co.kr>
2021-11-23 07:58:36 +0900

    refs #17 add project1, project2

---
+++ src/components/Project2.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1> PROJECT2 </h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Project2'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+  border: solid;
+  width: 100%;
+  height: 500px;
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,18 +1,23 @@
 <template>
   <section>
     <div class="content">
+      <Project1/>
+      <Project2/>
     </div>
   </section>
 </template>
 
 <script>
+import Project1 from './Project1.vue'
+import Project2 from './Project2.vue'
 export default {
   name: 'Projects',
   components: {
+    Project1,
+    Project2
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 section {
@@ -27,5 +32,6 @@ section {
   justify-content: space-around;
   align-items: center;
   flex-wrap: wrap;
+  gap: 24px;
 }
 </style>
