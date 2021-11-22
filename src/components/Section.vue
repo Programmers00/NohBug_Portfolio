7a6249b8430b4753b6b189f600c3e5af5017ac7b
노은신 <nes0410@i-gns.co.kr>
2021-11-23 06:34:42 +0900

    refs #13 add padding 24px

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -22,13 +22,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 section {
-  
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 24px;
 }
 .content {
-  
   width: 1200px;
   display: flex;
   justify-content: space-around;
