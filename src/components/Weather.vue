4f83760ea71da832d15c95fb0bb38e0c69e82976
노은신 <nes0410@i-gns.co.kr>
2021-11-25 22:54:35 +0900

    refs #18 geolocation, openweathermap API connected

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -2,15 +2,15 @@
   <div class="weather">
     <div>
       <h2>Weather</h2>
-      <h3>Check the current weather of your city</h3>
-      <input v-model="request.city_name">
-      <button @click="checkWeather">Check Weather</button>
+      <!-- <input v-model="request.city_name">
+      <button @click="checkWeather">Check Weather</button> -->
     </div>
     <div>
       <h2>{{response.degree}}°C</h2>
       <h3>{{response.weather}}</h3>
-      <img src="" alt="">
-      <h3>{{response.location}}</h3>
+      <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather">
+      <h3>{{response.city}}</h3>
+      <h3>{{response.country}}</h3>
     </div>
   </div>
 </template>
@@ -21,23 +21,45 @@ export default {
     data(){
       return {
         request: {
-          base_url: 'api.openweathermap.org/data/2.5/',
+          base_url: 'https://api.openweathermap.org/data/2.5/',
           city_name: 'seoul',
           api_key: 'b81580e434628d884e50b02bdd2effb8'
         },
         response: {
-          degree: '15',
-          weather: 'Clouds',
+          degree: '',
+          weather: '',
           icon: '',
-          location: 'Busan, KR'
+          city: '',
+          country: '',
+          latitude: '',
+          longitude: ''
         }
       } 
     },
+    created() {
+      navigator.geolocation.getCurrentPosition(location => {
+        this.latitude = location.coords.latitude
+        this.longitude = location.coords.longitude
+        console.log(this.latitude, this.longitude)
+      })
+    },
+    mounted() {
+      this.checkWeather()
+    },
   methods:{
     checkWeather() {
-      setTimeout(() => { fetch(`${this.request.base_url}weather?q=${this.request.city_name}&appid=${this.request.api_key}`)
-      .then(response => response)
-      .then(result => this.results(result)) }, 1000)
+      setTimeout(() => { 
+        fetch(`${this.request.base_url}weather?lat=${this.latitude}&lon=${this.longitude}&appid=${this.request.api_key}`)
+          .then(response => response.json())
+          .then(result => this.results(result))
+          }, 1000)
+    },
+    results(result) {
+      this.response.degree = Math.round(result.main.temp-273) //Kelvin - 273 = Celscius
+      this.response.weather = result.weather[0].main 
+      this.response.icon = `http://openweathermap.org/img/wn/${this.result.weather[0].icon}@2x.png`
+      this.response.city = result.name
+      this.response.country = result.sys.country
     }
   }
 }
