a5406e5e45887c1e86b79ef5ac28b53a4d743f3e
노은신 <nes0410@i-gns.co.kr>
2021-12-09 22:03:58 +0900

    refs #21 only slide display

--- src/components/Project2.vue
+++ src/components/Project2.vue
@@ -1,7 +1,33 @@
 <template>
   <section>
-    <div class="content">
-      <h1> PROJECT2 </h1>
+    <input type="radio" name="slide" id="slide01" checked>
+    <input type="radio" name="slide" id="slide02">
+    <input type="radio" name="slide" id="slide03">
+
+    <div class="slide__wrap">
+      <ul class="slide__list">
+        <li>
+          <a href="">
+            <label for="slide03" class="left"></label>
+            <img src="@/assets/images/slide01.jpg" alt="">
+            <label for="slide02" class="right"></label>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <label for="slide01" class="left"></label>
+            <img src="@/assets/images/slide02.jpg" alt="">
+            <label for="slide03" class="right"></label>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <label for="slide02" class="left"></label>
+            <img src="@/assets/images/slide03.jpg" alt="">
+            <label for="slide01" class="right"></label>
+          </a>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -26,6 +52,47 @@ section {
   box-shadow: 2px 2px 2px 2px whitesmoke;
   opacity: .9;
   transition-timing-function: ease-out;
+}
+section input[id*="slide"] {
+  display: none;
+}
+section .slide__wrap {
+  max-width: 1200px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+section .slide__list {
+  white-space: nowrap;
+  font-size: 0;
+}
+section .slide__list > li {
+  display: inline-block;
+  vertical-align: middle;
+  width: 100%;
+  transition: all .5s;
+}
+section .slide__list > li > a {
+  display: block;
+  position: relative;
+}
+section .slide__list > li > a img {
+  width: 100%;
+}
+section .slide__list label {
+  position: absolute;
+  z-index:999;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 50px;
+  cursor: pointer;
+}
+section .slide__list .left {
+  left: 30px;
+  background:url('../assets/images/left.png') center center / 100% no-repeat;
+}
+section .slide__list .right {
+  right: 30px;
+  background:url('../assets/images/right.png') center center / 100% no-repeat;
 }
 /* section:hover {
   border: 3px solid #263343;
