c74f607fe9b47e3fa3c37418ecd173f1188d1ce5
Eunshin Noh <nes0410@gmail.com>
2022-03-16 22:37:36 +0900

    [AG-GRID] developing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/router/index'
+import router from '@/router/index' //router
+
+import 'ag-grid-enterprise' //ag-grid enterprise function
 
 createApp(App).use(router).mount('#app') //setting
--- src/views/projects/noticeBoard/Index.vue
+++ src/views/projects/noticeBoard/Index.vue
@@ -1,14 +1,21 @@
 <template>
   <section>
-      <ag-grid-vue style="width: 500px; height: 500px;"
+    <button @click="getSelectedRows()">Get Selected Rows</button>
+      <ag-grid-vue 
+        
         class="ag-theme-alpine"
         :columnDefs="columnDefs"
-        :rowData="rowData">
+        :rowData="rowData"
+        rowSelection="multiple"
+        autoGroupColumnDef="autoGroupColumnDef"
+        @grid-ready="onGridReady"
+        >
     </ag-grid-vue>
   </section>
 </template>
 
 <script>
+import {reactive, ref, onMounted} from "vue"
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import { AgGridVue } from "ag-grid-vue3";
@@ -18,18 +25,51 @@ export default {
   components: {
      AgGridVue
   },
-  data() {
+  setup() {
+    let rowData = reactive([])
+    let gridApi = ref(null)
+    // eslint-disable-next-line no-unused-vars
+    let columnApi = ref(null)
+    
+    onMounted(() => {
+        fetch('https://www.ag-grid.com/example-assets/row-data.json')
+          .then(result => result.json())
+          .then(remoteRowData => rowData.value = remoteRowData);
+    })
+
+    const onGridReady = params => {
+      gridApi = params.api;
+      columnApi = params.columnApi;
+    }
+
+    const getSelectedRows = () => {
+      const selectedNodes = gridApi.value.getSelectedNodes()
+      const selectedData = selectedNodes.map( node => node.data )
+      const selectedDataStringPresentation = selectedData.map( node => `${node.make} ${node.model}`).join(', ')
+      alert(`Selected nodes: ${selectedDataStringPresentation}`)
+    }
     return {
       columnDefs: [
-        { headerName: "Make", field: "make" },
-        { headerName: "Model", field: "model" },
-        { headerName: "Price", field: "price" },
+        { headerName: "Make", field: "make", sortable: true, filter: true, checkboxSelection: true, rowGroup: true},
+        { headerName: "Model", field: "model", sortable: true, filter: true}, 
+        { headerName: "Price", field: "price", sortable: true, filter: true},
       ],
-      rowData: [
-        { make: "Toyota", model: "Celica", price: 35000 },
-        { make: "Ford", model: "Mondeo", price: 32000 },
-        { make: "Porsche", model: "Boxter", price: 72000 },
-      ]
+      rowData,
+      // rowData: [
+      //   { make: "Toyota", model: "Celica", price: 35000 },
+      //   { make: "Ford", model: "Mondeo", price: 32000 },
+      //   { make: "Porsche", model: "Boxter", price: 72000 },
+      // ],
+      autoGroupColumnDef: {
+        headerName: 'Model',
+        field: 'model',
+        cellRenderer: 'agGroupCellRenderer',
+        cellRendererParams: {
+          checkbox: true
+      }
+     },
+      onGridReady,
+      getSelectedRows
     }
   }
 }
@@ -37,9 +77,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  @import "~ag-grid-community/dist/styles/ag-grid.css";
-  @import "~ag-grid-community/dist/styles/ag-theme-alpine.css";
+  @import "~ag-grid-community/dist/styles/ag-grid.css"; //ag-grid style
+  @import "~ag-grid-community/dist/styles/ag-theme-alpine.css"; //ag-grid theme
 
+.ag-theme-alpine {
+  width: 700px;
+  height: 700px;
+//   // @include ag-theme-alpine((
+//   // ));
+}
 section {
   display: flex;
   flex-wrap: wrap;
